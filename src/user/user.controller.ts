b5c04ac80b8ee5ba0c6f29b592a6bc8b1b88a389
mindkeeper <fcb.nyak@gmail.com>
2023-08-16 21:04:26 +0700

    applying custom decorator GetUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Req, UseFilters, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseFilters, UseGuards } from '@nestjs/common';
 import { HttpExceptionFilter } from 'src/global-filters';
-import { Request } from 'express';
 import { jwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
 @UseFilters(HttpExceptionFilter)
 @Controller('users')
 export class UserController {
   @UseGuards(jwtGuard)
   @Get()
-  getUser(@Req() req: Request) {
-    return { user: req.user };
+  getUser(@GetUser() user: User) {
+    return { user };
   }
 }
